a5b75abd55f7b4ba9be939219ee450e2777482bb
Warorua <waroruaalex640@gmail.com>
2023-11-30 00:01:01 +0300

    kyf

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oops!!! Seems I'm hacked</title></head>
-<body>
-    <img align="center" width="100%" src="img/zac-efron-not-mad-just-disappointed-wn8fhcqbpq1kgmeu.webp"></img>
-</body>
-</html>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
